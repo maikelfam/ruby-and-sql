5752bba535602b44edf3706660147202c48e15e8
maikelfam <maikel.fam@kellogg.northwestern.edu>
2022-02-01 17:25:51 +0000

    did part of final lab

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_180405) do
+ActiveRecord::Schema.define(version: 2022_02_01_172235) do
+
+  create_table "activities", force: :cascade do |t|
+    t.string "created_when"
+    t.string "notes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -28,6 +35,14 @@ ActiveRecord::Schema.define(version: 2022_01_31_180405) do
     t.string "email"
     t.string "phone_number"
     t.integer "company_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "salespeople", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
