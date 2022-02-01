8a677db2b0bec4dd0f5bc83392eff299b54acff7
maikelfam <maikel.fam@kellogg.northwestern.edu>
2022-02-01 14:50:39 +0000

    did classes code along

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -3,6 +3,25 @@
 # should be the top-level directory (i.e. /workspace/your-app/) and then run:
 # rails runner code-along/0-classes.rb
 
+brians_favorite_food = "tacos"
+bens_favorite_food = "sushi"
+
+puts brians_favorite_food.class
+puts brians_favorite_food.upcase
+puts bens_favorite_food.upcase
+
+number_of_tacos = 5
+puts number_of_tacos.class
 # String Class
 
 # String Methods
+class Dog
+
+    def speak
+        puts "woof"
+    end 
+
+end 
+
+jenkins = Dog.new
+jenkins.speak
